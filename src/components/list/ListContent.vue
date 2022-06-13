2795954424c65a44c6e71b21f0a3ca80a008ded7
Yiang <yiangforwork@gmail.com>
2022-06-14 00:04:00 +0800

    refactor & add: hide completed tasks feature

---
+++ src/components/list/ListContent.vue
@@ -0,0 +1,150 @@
+<style scoped>
+.task-list {
+    overflow-y: auto;
+    padding-right: 5px;
+}
+
+.task-list::-webkit-scrollbar {
+    width: 8px;
+    margin-left: 1rem;
+}
+
+.task-list::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    background: rgba(0, 0, 0, 0.3);
+}
+
+.task-input {
+    background: rgba(0, 0, 0, 0.2);
+    color: #FFF;
+}
+
+.task-list.nothing-found {
+    align-items: center;
+    justify-content: center;
+}
+
+.completed-toggle {
+    background: rgba(0, 0, 0, 0.5);
+    padding: .5rem 1rem;
+    border-radius: 5px;
+}
+
+.icon-svg.showCompleted {
+    transform: rotate(180deg);
+}
+</style>
+
+<template>
+    <div class="inner h-full flex flex-col gap-8" v-if="list">
+        <header class="list-header">
+            <h2 class="text-4xl font-bold text-white">
+                <GhostInput
+                    :value="list.name"
+                />
+            </h2>
+        </header>
+
+        <div
+            :class="['task-list flex flex-col gap-2 task-list flex-1', list.tasks.length === 0 ? 'nothing-found' : '' ]"
+        >
+            <div
+                class="text-white text-3xl flex items-center content-center flex-col"
+                v-if="list.tasks.length === 0"
+            >
+                <img
+                    src="@/assets/cute.svg"
+                    alt="cute"
+                    width="100"
+                >
+                <h2 class="mt-4">Let's add some task</h2>
+            </div>
+
+            <template v-else>
+                <TaskItem
+                    v-for="task in normalTasks"
+                    :key="task.id"
+                    :task="task"
+                    @click="toggleTaskDetail(task)"
+                    @keydown.enter="toggleTaskDetail(task)"
+                />
+
+                <div class="flex">
+                    <button
+                        class="completed-toggle text-white flex items-center"
+                        @click="showCompleted = ! showCompleted"
+                    >
+                        <span>Completed</span>
+
+                        <Icon
+                            icon="ic:baseline-keyboard-arrow-down"
+                            :class="['icon-svg ml-2', showCompleted ? 'showCompleted' : '' ]"
+                        />
+                    </button>
+                </div>
+
+                <template v-if="showCompleted">
+                    <TaskItem
+                        v-for="task in completedTasks"
+                        :key="task.id"
+                        :task="task"
+                        @click="toggleTaskDetail(task)"
+                        @keydown.enter="toggleTaskDetail(task)"
+                    />
+                </template>
+            </template>
+        </div>
+
+        <AddNewInput
+            placeholder="Add a task"
+            @submit="addNewTask"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps} from "vue";
+import List from "@/core/model/List";
+import { Icon } from "@iconify/vue";
+import TaskItem from "./TaskItem.vue";
+import AddNewInput from "@/components/AddNewInput.vue";
+import GhostInput from "@/components/GhostInput.vue";
+import {ref, computed} from "vue";
+import type Task from "@/core/model/Task";
+import accessor from "@/core/accessor/AccessorInstance";
+import {useTodoStore} from "@/stores/todo";
+
+const todo = useTodoStore();
+const showCompleted = ref(false);
+
+const props = defineProps({
+    list: {
+        type: List,
+        required: true,
+    }
+});
+
+const normalTasks = computed(() => {
+    return props.list.tasks.filter(task => ! task.finish);
+});
+
+const completedTasks = computed(() => {
+    return props.list.tasks.filter(task => task.finish);
+});
+
+if( normalTasks.value.length === 0 ) {
+    showCompleted.value = true;
+}
+
+function addNewTask(name: string) {
+    if (props.list !== null) {
+        accessor.addTask(name, props.list.id).then(task => {
+            todo?.list?.tasks.push(task);
+        });
+    }
+}
+
+function toggleTaskDetail(task: Task) {
+    todo.toggleTask(task);
+}
+</script>
--- src/components/list/index.vue
+++ src/components/list/index.vue
@@ -2,99 +2,22 @@
 .task-list-wrap {
     background-color: #73d197;
 }
-
-.task-list {
-    overflow-y: auto;
-    padding-right: 5px;
-}
-
-.task-list::-webkit-scrollbar {
-    width: 8px;
-    margin-left: 1rem;
-}
-
-.task-list::-webkit-scrollbar-thumb {
-    border-radius: 5px;
-    background: rgba(0, 0, 0, 0.3);
-}
-
-.task-input {
-    background: rgba(0, 0, 0, 0.2);
-    color: #FFF;
-}
-
-.task-list.nothing-found {
-    align-items: center;
-    justify-content: center;
-}
 </style>
 
 <template>
     <div class="task-list-wrap p-5">
-        <div class="inner h-full flex flex-col gap-8" v-if="todo.list">
-            <header class="list-header">
-                <h2 class="text-4xl font-bold text-white">
-                    <GhostInput
-                        :value="todo.list.name"
-                    />
-                </h2>
-            </header>
-
-            <div
-                :class="['task-list flex flex-col gap-2 task-list flex-1', todo.list.tasks.length === 0 ? 'nothing-found' : '' ]"
-            >
-                <div
-                    class="text-white text-3xl flex items-center content-center flex-col"
-                    v-if="todo.list.tasks.length === 0"
-                >
-                    <img
-                        src="@/assets/cute.svg"
-                        alt="cute"
-                        width="100"
-                    >
-                    <h2 class="mt-4">Nothing Found</h2>
-                </div>
-
-                <TaskItem
-                    v-for="task in todo.list.tasks"
-                    :key="task.id"
-                    :task="task"
-                    @click="toggleTaskDetail(task)"
-                    @keydown.enter="toggleTaskDetail(task)"
-                />
-            </div>
-
-            <AddNewInput
-                placeholder="Add a task"
-                @submit="addNewTask"
-            />
-        </div>
+        <ListContent
+            v-if="todo.list"
+            :list="todo.list"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-import TaskItem from "./TaskItem.vue";
-import AddNewInput from "@/components/AddNewInput.vue";
-import GhostInput from "@/components/GhostInput.vue";
-import {provide, ref} from "vue";
-import type Task from "@/core/model/Task";
-import accessor from "@/core/accessor/AccessorInstance";
+import {provide} from "vue";
+import ListContent from "./ListContent.vue";
 import {useTodoStore} from "@/stores/todo";
-import {registerMenu} from "@/components/context_menu/data";
 
 const todo = useTodoStore();
-
 provide("icon-column-width", 50);
-
-function addNewTask(name: string) {
-    if (todo.list !== null) {
-        accessor.addTask(name, todo.list.id).then(task => {
-            todo?.list?.tasks.push(task);
-        });
-    }
-}
-
-function toggleTaskDetail(task: Task) {
-    todo.toggleTask(task);
-}
 </script>
