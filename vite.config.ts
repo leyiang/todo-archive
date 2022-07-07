b1f4099fff86834185f20ed0e29aa455f9329336
Yiang <yiangforwork@gmail.com>
2022-07-07 21:16:21 +0800

    add: unocss as css engine

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'url'
+import Unocss from "unocss/vite"
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [ vue(), Unocss() ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
