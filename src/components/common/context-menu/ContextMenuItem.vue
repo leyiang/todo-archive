4a98b05d10bcdc9928cdfbadf0d49e8d7a3820b2
Yiang <yiangforwork@gmail.com>
2022-07-17 09:18:05 +0800

    fix. contextmenu item style

--- src/components/common/context-menu/ContextMenuItem.vue
+++ src/components/common/context-menu/ContextMenuItem.vue
@@ -4,7 +4,8 @@
 
 <template>
     <button
-        class="px-1rem py-5px hover:(bg-gray-300) text-left"
+        btn-reset
+        class="px-1rem py-5px hover:bg-gray-300 text-left"
     >
         <slot></slot>
     </button>
