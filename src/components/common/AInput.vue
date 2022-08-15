c508148d6327c2189795dbb550670f34a90851df
Yiang <yiangforwork@gmail.com>
2022-08-15 21:12:08 +0800

    add: AInput v-model

--- src/components/common/AInput.vue
+++ src/components/common/AInput.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+defineProps({
+    modelValue: {
+        type: String
+    }
+});
+</script>
+
 <template>
     <input
         type="text"
         border-gray-300
         border-1
         rounded
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
     >
 </template>
